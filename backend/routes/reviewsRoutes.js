c6616f5c5a487e582b8bbeb95feeebda7ffa9b9c
Ganesh <ganeshnitalikar@gmail.com>
2025-11-14 11:51:06 +0530

    backend ready

--- backend/routes/reviewsRoutes.js
+++ backend/routes/reviewsRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const pool = require('../config/dbConfig')
-const result = require('../utils/result')
+const result = require('../utils/result');
+const { customAlphabet } = require("nanoid");
+const getFormatedDate = require("../utils/formatedDate");
 
 router.get('/reviews', (req, res) => {
     const { movieId } = req.body;
@@ -9,6 +11,28 @@ router.get('/reviews', (req, res) => {
     pool.query(sql, [movieId, req.headers.userId], (error, data) => {
         res.send(result.createResult(error, data));
     })
+})
+
+router.post('/reviews', (req, res) => {
+    const { review, rating , movieId} = req.body;
+    const sql = "INSERT INTO REVIEWS(id , movieId, review, rating , userId, modified) values (?,?,?,?,?,?)";
+     const generator = customAlphabet("1234567890", 4);
+    const id = generator();
+
+    const formattedTimestamp = getFormatedDate();
+    pool.query(sql, [id, movieId, review, rating, req.headers.userId, formattedTimestamp
+        
+    ], (error, data) => {
+        res.send(result.createResult(error, data));
+    });
+})
+
+router.delete('/reviews/:id', (req, res) => {
+    const id = req.params.id;
+  const sql = `DELETE FROM reviews where id = ?`
+  pool.query(sql, [id], (error, data) => {
+    res.send(result.createResult(error, data))
+  })
 })
 
 router.post('/reviews', (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const config = require("./utils/config");
 const result = require("./utils/result")
 const authorization = require('./routes/authorization');
 const reviewRoutes = require('./routes/reviewsRoutes');
+const movieRouter = require('./routes/movieRoutes')
 
 const userRouter = require('./routes/userRoutes')
 
@@ -18,7 +19,8 @@ app.use(authorization);
 
 
 app.use('/user', userRouter);
-app.use('/review',reviewRoutes )
+app.use('/review', reviewRoutes)
+app.use('/movie', movieRouter)
 
 app.listen(config.port, () => {
     console.log(`Server Started On Port ${config.port}`)
